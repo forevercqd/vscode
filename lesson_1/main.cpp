c7ca51fcbbe566c16e08cd3a86e4491e66a1dbbd
Robert <qingdong.cqd@alibaba-inc.com>
2018-10-27 17:42:39 +0800

    test: 上传第一个可正常运行的 vscode sample;

---
+++ lesson_1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 1;
+	int b = 2;
+//	printf("%s, line %d, %d.", __FUNCTION__, __LINE__, (a + b)	);
+
+	cout << "hello world" << endl; 
+
+//	return 0;
+}
